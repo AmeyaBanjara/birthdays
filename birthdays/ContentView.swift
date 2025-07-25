8563cbf4b62db59993a6e031c2c3fbe1da43092d
Ameya Banjara <ameyabanjara11@gmail.com>
2025-07-25 10:31:34 -0700

    added ui first commit

--- birthdays/ContentView.swift
+++ birthdays/ContentView.swift
@@ -8,17 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [friend] = [
+        Friend:(name: "Joan", birthday: .now),
+        Friend: (name: "Sienna", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            /*@START_MENU_TOKEN@*/Text("Content")/*@END_MENU_TOKEN@*/
         }
-        .padding()
+        
+        List(friends, id: /.name){ friend in
+            
+            HStack{
+                Text(friend.name)
+                Spacer()
+                Text(friend.birthday, format:
+                        .dateTime.month(.wide).day().year())
+            }
+        }
+        .navigationTitle("Birthdays")
+        .safeAreaInsert(edge: .bottom){
+            Vstack(alignment: .center, spacing: 20) {
+                Text("New Birthday")
+                    .font(.headline)
+                DatePicker(selection: $newBirthday, in:
+                           Date.distantPast...Date.now,
+                           displayedComponents: .date){
+                    TextField("Name" , text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                }
+                
+                
+                Button("Save"){
+                    let newFriend = Friend(name: newName,
+                                           birthday: newBirthday)
+                    friends.append(newFriend)
+                    newName = ""
+                    newBirthday = .now
+                }
+                .bold()
+            }
+            .padding()
+            .background(.bar)
+        }
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
